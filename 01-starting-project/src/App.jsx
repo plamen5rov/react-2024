c9bd28bdb1189f1608256bff82a0bd4251432c93
Plamen Petrov <biznetmen@gmail.com>
2023-11-27 11:28:43 +0200

    Alternative Props Syntaxes

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,7 @@
 /** @format */
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
+
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -42,24 +43,24 @@ function App() {
 					<h2>Core Concepts</h2>
 					<ul>
 						<CoreConcept
-							title='Components'
-							description='The core UI building block.'
-							image={componentsImg}
+							title={CORE_CONCEPTS[0].title}
+							description={CORE_CONCEPTS[0].description}
+							image={CORE_CONCEPTS[0].image}
 						/>
 						<CoreConcept
-							title='Components'
-							description='The core UI building block.'
-							image={componentsImg}
+							title={CORE_CONCEPTS[1].title}
+							description={CORE_CONCEPTS[1].description}
+							image={CORE_CONCEPTS[1].image}
 						/>
 						<CoreConcept
-							title='Components'
-							description='The core UI building block.'
-							image={componentsImg}
+							title={CORE_CONCEPTS[2].title}
+							description={CORE_CONCEPTS[2].description}
+							image={CORE_CONCEPTS[2].image}
 						/>
 						<CoreConcept
-							title='Components'
-							description='The core UI building block.'
-							image={componentsImg}
+							title={CORE_CONCEPTS[3].title}
+							description={CORE_CONCEPTS[3].description}
+							image={CORE_CONCEPTS[3].image}
 						/>
 					</ul>
 				</section>
