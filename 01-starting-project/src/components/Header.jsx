87f2351b4a7b93a27deb219a028a0e08db246f0f
Plamen Petrov <biznetmen@gmail.com>
2023-11-28 11:10:05 +0200

    Best Practice: Storing Components in Files & Using a Good Project Structure

---
+++ 01-starting-project/src/components/Header.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+import reactImg from '../assets/react-core-concepts.png';
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+	return Math.floor(Math.random() * (max + 1));
+}
+
+const description = reactDescriptions[genRandomInt(2)];
+
+export default function Header() {
+	return (
+		<header>
+			<img src={reactImg} alt='Stylized atom' />
+			<h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you are
+				going to build!
+			</p>
+		</header>
+	);
+}
