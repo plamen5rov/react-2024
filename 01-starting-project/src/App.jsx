1c0047c216020fd5a59acfe7bf3b0248cdedfba7
Plamen Petrov <biznetmen@gmail.com>
2023-11-29 12:14:38 +0200

    Rendering Content Conditionally

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,7 +7,7 @@ import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
-	const [selectedTopic, setSelectedTopic] = useState('components');
+	const [selectedTopic, setSelectedTopic] = useState();
 
 	function handleSelect(selectedButton) {
 		setSelectedTopic(selectedButton);
@@ -36,13 +36,16 @@ function App() {
 						<TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
 						<TabButton onSelect={() => handleSelect('state')}>State</TabButton>
 					</menu>
-					<div id='tab-content'>
-						<h3>{EXAMPLES[selectedTopic].title}</h3>
-						<p>{EXAMPLES[selectedTopic].description}</p>
-						<pre>
-							<code>{EXAMPLES[selectedTopic].code}</code>
-						</pre>
-					</div>
+					{!selectedTopic ? <p>Please, select a topic!</p> : null}
+					{selectedTopic && (
+						<div id='tab-content'>
+							<h3>{EXAMPLES[selectedTopic].title}</h3>
+							<p>{EXAMPLES[selectedTopic].description}</p>
+							<pre>
+								<code>{EXAMPLES[selectedTopic].code}</code>
+							</pre>
+						</div>
+					)}
 				</section>
 				<h2>Time to get started!</h2>
 			</main>
