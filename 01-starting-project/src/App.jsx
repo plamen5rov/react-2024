7fae1bb1ed648f052bfc4ea7b029d51f388fa15f
Plamen Petrov <biznetmen@gmail.com>
2023-11-29 11:27:31 +0200

    Managing State & Using Hooks

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,17 @@
 /** @format */
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+	const [selectedTopic, setSelectedTopic] = useState('Please, click a button!');
+
 	function handleSelect(selectedButton) {
-		console.log(selectedButton);
+		setSelectedTopic(selectedButton);
+		//console.log(selectedTopic);
 	}
 	return (
 		<div>
@@ -31,6 +36,7 @@ function App() {
 						<TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
 						<TabButton onSelect={() => handleSelect('state')}>State</TabButton>
 					</menu>
+					{selectedTopic}
 				</section>
 				<h2>Time to get started!</h2>
 			</main>
