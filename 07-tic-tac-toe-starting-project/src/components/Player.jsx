2efde566c5f9f45962124f02e89e16e5e6b31fb2
Plamen Petrov <biznetmen@gmail.com>
2023-12-03 13:15:35 +0200

    Concept Repetition: Working with State

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,13 +1,30 @@
 /** @format */
+import { useState } from 'react';
 
 export default function Player({ name, symbol }) {
+	const [isEditing, setIsEditing] = useState(false);
+
+	const handleEdit = (e) => {
+		e.preventDefault();
+		setIsEditing(true);
+	};
+
+	let playerName = <span className='player-name'>{name}</span>;
+
+	if (isEditing) {
+		playerName = (
+			<span className='player-name'>
+				<input type='text' required />
+			</span>
+		);
+	}
+
 	return (
 		<li>
-			<span className='player'>
-				<span className='player-name'>{name}</span>
-				<span className='player-symbol'>{symbol}</span>
-			</span>
-			<button>Edit</button>
+			{playerName}
+			<span className='player-symbol'>{symbol}</span>
+
+			<button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
 		</li>
 	);
 }
