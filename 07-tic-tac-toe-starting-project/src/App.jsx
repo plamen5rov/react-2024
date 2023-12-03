d3353c02146ef6d127ddb93c25c3d275a51a37f3
Plamen Petrov <biznetmen@gmail.com>
2023-12-03 08:16:11 +0200

    First Steps Towards Our Tic-Tac-Toe Game

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,7 +1,24 @@
 /** @format */
 
 function App() {
-	return <h1>Coming soon...</h1>;
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<li>
+						<span className='player-name'>Player 1</span>
+						<span className='player-symbol'>X</span>
+					</li>
+					<li>
+						<span className='player-name'>Player 2</span>
+						<span className='player-symbol'>O</span>
+					</li>
+				</ol>
+				GAME BOARD
+			</div>
+			LOG
+		</main>
+	);
 }
 
 export default App;
