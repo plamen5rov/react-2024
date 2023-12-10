904c1bfaea95502bf6da752c3c8501a79aa418ac
Plamen Petrov <biznetmen@gmail.com>
2023-12-10 13:19:04 +0200

    Why Immutability Matters - Always!

--- 07-tic-tac-toe-starting-project/src/components/GameOver.jsx
+++ 07-tic-tac-toe-starting-project/src/components/GameOver.jsx
@@ -1,13 +1,13 @@
 /** @format */
 
-export default function GameOver({ winner }) {
+export default function GameOver({ winner, onRestart }) {
 	return (
 		<div id='game-over'>
 			<h2>Game Over</h2>
 			{winner && <p>{winner} won!</p>}
 			{!winner && <p>It's a draw!</p>}
 			<p>
-				<button>Restart</button>
+				<button onClick={onRestart}>Restart</button>
 			</p>
 		</div>
 	);
