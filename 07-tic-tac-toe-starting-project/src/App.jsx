f44ddd143f4c016af81438261217bea980800d0d
Plamen Petrov <biznetmen@gmail.com>
2023-12-10 13:11:06 +0200

    The "Game Over" Screen & Checking for a Draw

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,7 @@ import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './winning-combinations.js';
+import GameOver from './components/GameOver.jsx';
 
 const initialGameBoard = [
 	[null, null, null],
@@ -55,6 +56,8 @@ function App() {
 		}
 	}
 
+	const hasDraw = gameTurns.length === 9 && !winner;
+
 	function handleSelectSquare(rowIndex, colIndex) {
 		// setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'));
 		setGameTurns((prevTurns) => {
@@ -83,7 +86,7 @@ function App() {
 						isActive={activePlayer === 'O'}
 					/>
 				</ol>
-				{winner && <h2>You won, {winner}!</h2>}
+				{(winner || hasDraw) && <GameOver winner={winner} />}
 				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
 			</div>
 			<Log turns={gameTurns} />
