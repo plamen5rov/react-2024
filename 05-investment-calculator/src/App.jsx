82262e868141665c8d98ca319ccfd6cb284e5952
Plamen Petrov <biznetmen@gmail.com>
2023-12-15 19:20:28 +0200

    Outputting Content Conditionally

--- 05-investment-calculator/src/App.jsx
+++ 05-investment-calculator/src/App.jsx
@@ -13,6 +13,8 @@ function App() {
 		duration: 10,
 	});
 
+	const inputIsValid = userInput.duration >= 1;
+
 	function handleChange(inputIdentifier, newValue) {
 		setUserInput((prevUserInput) => {
 			return {
@@ -26,7 +28,8 @@ function App() {
 		<>
 			<Header />
 			<UserInput userInput={userInput} onChange={handleChange} />
-			<Results input={userInput} />
+			{!inputIsValid && <p id='header'>Please, input valid data!</p>}
+			{inputIsValid && <Results input={userInput} />}
 		</>
 	);
 }
