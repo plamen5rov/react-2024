aaea03db267927fc3c116bf319989e65b410b2c6
Plamen Petrov <biznetmen@gmail.com>
2023-12-16 10:42:52 +0200

    Introducing Tailwind CSS For React App Styling

--- 07-styling-project/tailwind.config.js
+++ 07-styling-project/tailwind.config.js
@@ -1,9 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
 
+export default {
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
