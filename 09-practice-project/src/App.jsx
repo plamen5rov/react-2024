b270544805a38b7cf3e14f48bd300b78a00e8a19
Plamen Petrov <biznetmen@gmail.com>
2024-01-11 16:49:14 +0200

    Handling Project Deletion

--- 09-practice-project/src/App.jsx
+++ 09-practice-project/src/App.jsx
@@ -48,12 +48,25 @@ function App() {
 			};
 		});
 	}
+	function handleDeleteProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectID: undefined,
+				projects: prevState.projects.filter(
+					(project) => project.id !== prevState.selectedProjectID
+				),
+			};
+		});
+	}
 
 	const selectedProject = projectsState.projects.find(
 		(project) => project.id === projectsState.selectedProjectID
 	);
 
-	let content = <SelectedProject project={selectedProject} />;
+	let content = (
+		<SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+	);
 
 	//console.log(projectsState);
 
