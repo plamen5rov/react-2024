254625248aaf3203cfaa28e0bb81ec0c2e0b7f7e
Plamen Petrov <biznetmen@gmail.com>
2024-01-14 15:01:03 +0200

    useEffect Not Needed: Another Example

--- 11-side-effects/src/App.jsx
+++ 11-side-effects/src/App.jsx
@@ -9,11 +9,16 @@ import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
 import { sortPlacesByDistance } from './loc.js';
 
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map((id) =>
+	AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
 function App() {
 	const modal = useRef();
 	const selectedPlace = useRef();
 	const [availablePlaces, setAvailablePlaces] = useState([]);
-	const [pickedPlaces, setPickedPlaces] = useState([]);
+	const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition((position) => {
@@ -44,9 +49,13 @@ function App() {
 			return [place, ...prevPickedPlaces];
 		});
 
-    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-    if (storedIds.indexOf(id) === -1) {
-		localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+		const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+		if (storedIds.indexOf(id) === -1) {
+			localStorage.setItem(
+				'selectedPlaces',
+				JSON.stringify([id, ...storedIds])
+			);
+		}
 	}
 
 	function handleRemovePlace() {
@@ -54,6 +63,12 @@ function App() {
 			prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
 		);
 		modal.current.close();
+
+		const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+		localStorage.setItem(
+			'selectedPlaces',
+			JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+		);
 	}
 
 	return (
