62127b8b1d03721e0c0789f79ee94ef5a335da86
Plamen Petrov <biznetmen@gmail.com>
2024-01-15 17:56:03 +0200

    Preparing Another Use-Case For useEffect

--- 11-side-effects/src/components/Modal.jsx
+++ 11-side-effects/src/components/Modal.jsx
@@ -1,26 +1,17 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
+/** @format */
+
+import { useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = forwardRef(function Modal({ children }, ref) {
-  const dialog = useRef();
+function Modal({ open, children }) {
+	const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-      close: () => {
-        dialog.current.close();
-      },
-    };
-  });
-
-  return createPortal(
-    <dialog className="modal" ref={dialog}>
-      {children}
-    </dialog>,
-    document.getElementById('modal')
-  );
-});
+	return createPortal(
+		<dialog className='modal' ref={dialog} open={open}>
+			{children}
+		</dialog>,
+		document.getElementById('modal')
+	);
+}
 
 export default Modal;
