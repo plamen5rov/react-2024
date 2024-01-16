1bc855bea197fdc5d659ca399fce39c9948d791a
Plamen Petrov <biznetmen@gmail.com>
2024-01-16 10:28:55 +0200

    Fixing a Small Bug

--- 11-side-effects/src/components/Modal.jsx
+++ 11-side-effects/src/components/Modal.jsx
@@ -3,7 +3,7 @@
 import { useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
 	const dialog = useRef();
 
 	useEffect(() => {
@@ -12,10 +12,10 @@ function Modal({ open, children }) {
 		} else {
 			dialog.current.close();
 		}
-	}, []);
+	}, [open]);
 
 	return createPortal(
-		<dialog className='modal' ref={dialog} open={open}>
+		<dialog className='modal' ref={dialog} onClose={onClose}>
 			{children}
 		</dialog>,
 		document.getElementById('modal')
