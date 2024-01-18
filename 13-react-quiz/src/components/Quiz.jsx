23b33bb5565f4c11183eeda0a7920eab80f41055
Plamen Petrov <biznetmen@gmail.com>
2024-01-18 13:05:44 +0200

    Adding Question Timers

--- 13-react-quiz/src/components/Quiz.jsx
+++ 13-react-quiz/src/components/Quiz.jsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
+import QuestionsTimer from './QuestionsTimer.jsx';
+
 function Quiz() {
 	const [userAnswers, setUserAnswers] = useState([]);
 
@@ -29,6 +31,10 @@ function Quiz() {
 	return (
 		<div id='quiz'>
 			<div id='question'>
+				<QuestionsTimer
+					timeout={10000}
+					onTimeout={() => handleSelectAnswer(null)}
+				/>
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 				<ul id='answers'>
 					{shuffledAnswers.map((answer) => (
