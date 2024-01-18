268ff7e576ad625f34c868612cbd24f61e7b0648
Plamen Petrov <biznetmen@gmail.com>
2024-01-18 10:42:52 +0200

    Deriving Values, Outputting Questions & Registering Answers

--- 13-react-quiz/src/App.jsx
+++ 13-react-quiz/src/App.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
 import Header from './components/Header';
+import Quiz from './components/Quiz';
 
 function App() {
 	return (
 		<>
 			<Header />
+			<main>
+				<Quiz />
+			</main>
 		</>
 	);
 }
--- 13-react-quiz/src/components/Quiz.jsx
+++ 13-react-quiz/src/components/Quiz.jsx
@@ -1,9 +1,33 @@
 /** @format */
 import { useState } from 'react';
+import QUESTIONS from '../questions.js';
 function Quiz() {
-	const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
 	const [userAnswers, setUserAnswers] = useState([]);
-	return <p>Currently active Question</p>;
+
+	const activeQuestionIndex = userAnswers.length;
+
+	function handleSelectAnswer(selectedAnswer) {
+		setUserAnswers((prevUserAnswers) => {
+			return [...prevUserAnswers, selectedAnswer];
+		});
+	}
+
+	return (
+		<div id='quiz'>
+			<div id='question'>
+				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+				<ul id='answers'>
+					{QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+						<li key={answer} className='answer'>
+							<button onClick={() => handleSelectAnswer(answer)}>
+								{answer}
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default Quiz;
