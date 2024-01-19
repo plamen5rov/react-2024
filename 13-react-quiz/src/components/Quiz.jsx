cc35d97d28dbb413fcdfa3f65f39552165f6d2f6
Plamen Petrov <biznetmen@gmail.com>
2024-01-19 11:40:40 +0200

    Working with Effect Dependencies & useCallback

--- 13-react-quiz/src/components/Quiz.jsx
+++ 13-react-quiz/src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 /** @format */
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
 import QuestionsTimer from './QuestionsTimer.jsx';
@@ -11,11 +11,17 @@ function Quiz() {
 
 	const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-	function handleSelectAnswer(selectedAnswer) {
+	const handleSelectAnswer = useCallback(function handleSelectAnswer(
+		selectedAnswer
+	) {
 		setUserAnswers((prevUserAnswers) => {
 			return [...prevUserAnswers, selectedAnswer];
 		});
-	}
+	});
+
+	const handleSkipAnswer = useCallback(() => {
+		() => handleSelectAnswer(null), [handleSelectAnswer];
+	}, []);
 
 	if (quizIsComplete) {
 		return (
@@ -31,17 +37,12 @@ function Quiz() {
 	return (
 		<div id='quiz'>
 			<div id='question'>
-				<QuestionsTimer
-					timeout={10000}
-					onTimeout={() => handleSelectAnswer(null)}
-				/>
+				<QuestionsTimer timeout={10000} onTimeout={handleSkipAnswer} />
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 				<ul id='answers'>
 					{shuffledAnswers.map((answer) => (
 						<li key={answer} className='answer'>
-							<button onClick={() => handleSelectAnswer(answer)}>
-								{answer}
-							</button>
+							<button onClick={handleSelectAnswer}>{answer}</button>
 						</li>
 					))}
 				</ul>
