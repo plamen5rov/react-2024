df7821c063beb2176f067aa6fdf3c665f29f2f04
Plamen Petrov <biznetmen@gmail.com>
2024-01-21 13:00:10 +0200

    Understanding the useMemo() Hook

--- 12-BEHIND-THE-SCENES/src/components/Counter/Counter.jsx
+++ 12-BEHIND-THE-SCENES/src/components/Counter/Counter.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { useState, memo, useCallback } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -27,7 +27,10 @@ function isPrime(number) {
 
 const Counter = memo(function Counter({ initialCount }) {
 	log('<Counter /> rendered', 1);
-	const initialCountIsPrime = isPrime(initialCount);
+	const initialCountIsPrime = useMemo(
+		() => isPrime(initialCount),
+		[initialCount]
+	);
 
 	const [counter, setCounter] = useState(initialCount);
 
