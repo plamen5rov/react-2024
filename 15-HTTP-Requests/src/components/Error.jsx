04a4bcd56dfe3284438e8ca9b8406849a26cf708
Plamen Petrov <biznetmen@gmail.com>
2024-01-25 11:58:01 +0200

    Handling HTTP Errors

---
+++ 15-HTTP-Requests/src/components/Error.jsx
@@ -0,0 +1,17 @@
+/** @format */
+
+export default function Error({ title, message }) {
+	return (
+		<div className='error'>
+			<h2>{title}</h2>
+			<p>{message}</p>
+			{/* {onConfirm && (
+				<div id='confirmation-actions'>
+					<button onClick={onConfirm} className='button'>
+						Okay
+					</button>
+				</div>
+			)} */}
+		</div>
+	);
+}
