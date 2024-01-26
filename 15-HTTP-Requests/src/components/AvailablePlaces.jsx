95ebbd9b862615be3c92a0e60bae3ead04472734
Plamen Petrov <biznetmen@gmail.com>
2024-01-26 16:44:00 +0200

    Transforming Fetched Data

--- 15-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -3,6 +3,7 @@
 import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
 import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [isFetching, setIsFetching] = useState(false);
@@ -14,27 +15,39 @@ export default function AvailablePlaces({ onSelectPlace }) {
 			try {
 				const response = await fetch('http://localhost:3000/places');
 				const resData = await response.json();
-
+				// console.log(resData.places);
 				if (!response.ok) {
 					throw new Error('Failed to fetch places');
 				}
 				setAvailablePlaces(resData.places);
+				setIsFetching(false);
+				// navigator.geolocation.getCurrentPosition((position) => {
+				// 	const sortedPlaces = sortPlacesByDistance(
+				// 		resData.places,
+				// 		position.coords.latitude,
+				// 		position.coords.longitude
+				// 	);
+				// 	console.log(`Sorted: ${sortedPlaces}`);
+				// 	setAvailablePlaces(resData.places); // used to be setAvailablePlaces(sortedPlaces)
+				// 	setIsFetching(false);
+				// });
 			} catch (error) {
 				setError({
 					message:
 						error.message || 'Could not fetch places! Please, try again later!',
 				});
+				setIsFetching(false);
 			}
-
-			setIsFetching(false);
 		}
 
 		fetchPlaces();
 	}, []);
 
-	if (error) {
-		return <Error title='An error occurred!' message={error.message} />;
-	}
+	// if (error) {
+	// 	console.log(error);
+
+	// 	return <Error title='An error occurred!' message={error.message} />;
+	// }
 
 	return (
 		<Places
