195a28b0ceedcddd10d97b6a42a7020c790b6c57
Plamen Petrov <biznetmen@gmail.com>
2024-01-28 13:01:52 +0200

    Extracting Code & Improving Code Structure

--- 15-HTTP-Requests/src/components/AvailablePlaces.jsx
+++ 15-HTTP-Requests/src/components/AvailablePlaces.jsx
@@ -4,6 +4,7 @@ import Places from './Places.jsx';
 import { useState, useEffect } from 'react';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
+import { fetchAvailablePlaces } from '../http.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
 	const [isFetching, setIsFetching] = useState(false);
@@ -13,24 +14,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
 		async function fetchPlaces() {
 			setIsFetching(true);
 			try {
-				const response = await fetch('http://localhost:3000/places');
-				const resData = await response.json();
-				// console.log(resData.places);
-				if (!response.ok) {
-					throw new Error('Failed to fetch places');
-				}
-				setAvailablePlaces(resData.places);
+				const places = await fetchAvailablePlaces();
+				setAvailablePlaces(places);
 				setIsFetching(false);
-				// navigator.geolocation.getCurrentPosition((position) => {
-				// 	const sortedPlaces = sortPlacesByDistance(
-				// 		resData.places,
-				// 		position.coords.latitude,
-				// 		position.coords.longitude
-				// 	);
-				// 	console.log(`Sorted: ${sortedPlaces}`);
-				// 	setAvailablePlaces(resData.places); // used to be setAvailablePlaces(sortedPlaces)
-				// 	setIsFetching(false);
-				// });
+				navigator.geolocation.getCurrentPosition((position) => {
+					const sortedPlaces = sortPlacesByDistance(
+						places,
+						position.coords.latitude,
+						position.coords.longitude
+					);
+					console.log(`Sorted: ${sortedPlaces}`);
+					setAvailablePlaces(sortedPlaces); // used to be setAvailablePlaces(sortedPlaces)
+					setIsFetching(false);
+				});
 			} catch (error) {
 				setError({
 					message:
@@ -43,16 +39,16 @@ export default function AvailablePlaces({ onSelectPlace }) {
 		fetchPlaces();
 	}, []);
 
-	// if (error) {
-	// 	console.log(error);
+	if (error) {
+		console.log(error);
 
-	// 	return <Error title='An error occurred!' message={error.message} />;
-	// }
+		return <Error title='An error occurred!' message={error.message} />;
+	}
 
 	return (
 		<Places
 			title='Available Places'
-			places={availablePlaces}
+			places={sortedPlaces}
 			isLoading={isFetching}
 			fallbackText='No places available.'
 			loadingText={'Fetching places data...'}
