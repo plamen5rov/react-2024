fb3c56cc0c0c17f1f563f8c4e733e135af01d704
Plamen Petrov <biznetmen@gmail.com>
2024-01-31 11:07:35 +0200

    Handling Form Submission

--- 17-forms-user-input/src/components/Login.jsx
+++ 17-forms-user-input/src/components/Login.jsx
@@ -1,24 +1,30 @@
+/** @format */
+
 export default function Login() {
-  return (
-    <form>
-      <h2>Login</h2>
+	function handleSubmit(event) {
+		event.preventDefault();
+		console.log('Submitted from form');
+	}
+	return (
+		<form onSubmit={handleSubmit}>
+			<h2>Login</h2>
 
-      <div className="control-row">
-        <div className="control no-margin">
-          <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
-        </div>
+			<div className='control-row'>
+				<div className='control no-margin'>
+					<label htmlFor='email'>Email</label>
+					<input id='email' type='email' name='email' />
+				</div>
 
-        <div className="control no-margin">
-          <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
-        </div>
-      </div>
+				<div className='control no-margin'>
+					<label htmlFor='password'>Password</label>
+					<input id='password' type='password' name='password' />
+				</div>
+			</div>
 
-      <p className="form-actions">
-        <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
-      </p>
-    </form>
-  );
+			<p className='form-actions'>
+				<button className='button button-flat'>Reset</button>
+				<button className='button'>Login</button>
+			</p>
+		</form>
+	);
 }
