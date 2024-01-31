10d8fd15491e6b26ce85b8d1cc1b3bc17bdeb189
Plamen Petrov <biznetmen@gmail.com>
2024-01-31 11:43:42 +0200

    Managing & Getting User Input via State & Generic Handlers

--- 17-forms-user-input/src/components/Login.jsx
+++ 17-forms-user-input/src/components/Login.jsx
@@ -1,10 +1,23 @@
 /** @format */
-
+import { useState } from 'react';
 export default function Login() {
+	const [enteredEmail, setEnteredEmail] = useState('');
+	const [enteredPassword, setEnteredPassword] = useState('');
+
+	function handleEmailChange(event) {
+		setEnteredEmail(event.target.value);
+	}
+
+	function handlePasswordChange(event) {
+		setEnteredPassword(event.target.value);
+	}
+
 	function handleSubmit(event) {
 		event.preventDefault();
-		console.log('Submitted from form');
+		console.log('User email: ' + enteredEmail);
+		console.log('User password: ' + enteredPassword);
 	}
+
 	return (
 		<form onSubmit={handleSubmit}>
 			<h2>Login</h2>
@@ -12,12 +25,24 @@ export default function Login() {
 			<div className='control-row'>
 				<div className='control no-margin'>
 					<label htmlFor='email'>Email</label>
-					<input id='email' type='email' name='email' />
+					<input
+						id='email'
+						type='email'
+						name='email'
+						onChange={handleEmailChange}
+						value={enteredEmail}
+					/>
 				</div>
 
 				<div className='control no-margin'>
 					<label htmlFor='password'>Password</label>
-					<input id='password' type='password' name='password' />
+					<input
+						id='password'
+						type='password'
+						name='password'
+						onChange={handlePasswordChange}
+						value={enteredPassword}
+					/>
 				</div>
 			</div>
 
